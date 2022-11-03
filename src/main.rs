ccbb612754c991570ce33cf183c51b55fc18d56d
Ghvstcode <impoissants@gmail.com>
2022-11-03 22:34:00 +0100

    init commit

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use std::{io, process};
+use io::Result;
+
+fn main() {
+    let result = run();
+
+    match result {
+        Err(error) => {
+            let stderr = io::stderr();
+            process::exit(1);
+        }
+        Ok(false) => {
+            process::exit(1);
+        }
+        Ok(true) => {
+            process::exit(0);
+        }
+    }
+}
+
+
+fn run() ->Result<bool> {
+  return Ok(true)
+}
